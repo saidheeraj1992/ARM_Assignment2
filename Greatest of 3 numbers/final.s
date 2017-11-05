5f5560e274df40118fda237c972889fc723061da
saidheeraj1992 <33399930+saidheeraj1992@users.noreply.github.com>
2017-11-06 00:09:14 +0530

    Update final.s

--- Greatest of 3 numbers/final.s
+++ Greatest of 3 numbers/final.s
@@ -3,14 +3,23 @@
    ENTRY
 __main function 
 	  
-	    MOV r1, #12
-		MOV r2, #4
-L1		CMP r1, r2	    
-		ITE LT
-        SUBLT r2, r2, r1; 
-		SUBGE r1, r1, r2;
-		BNE L1 ;
-		
+	    MOV r1, #6
+		MOV r2, #15
+		MOV r3, #4
+     	CMP r1, r2
+		BLE L1 ;
+		CMP r1, r3
+		BLE L2
+		MOV r4, r1
+		BAL L3 ;
+L1		CMP r2, r3
+		BLE L2
+		MOV r4, r2
+		BAL L3 ;
+  
+
+L2      MOV r4, r3 
+L3      
 stop  B stop
       endfunc
-		end  
+		end   
